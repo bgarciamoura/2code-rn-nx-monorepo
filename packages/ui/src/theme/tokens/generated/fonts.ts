3cb21e8c0ecaf17bc9a3cf7c8cff468b9cb6865b
Bruno Garcia <itsme@bgarciamoura.com>
2025-05-24 00:57:39 -0300

    feat: Included some tokens

--- packages/ui/src/theme/tokens/generated/fonts.ts
+++ packages/ui/src/theme/tokens/generated/fonts.ts
@@ -2,8 +2,8 @@
  * Do not edit directly, this file was auto-generated.
  */
 
-export const typographyFontFamiliesRoboto = "Roboto";
-export const typographyFontFamiliesInter = "Inter";
-export const typographyFontWeightsRoboto0 = "Regular";
-export const typographyFontWeightsInter1 = "Bold";
-export const typographyFontWeightsInter2 = "Regular";
+export const typographyFontFamiliesRoboto = 'Roboto';
+export const typographyFontFamiliesInter = 'Inter';
+export const typographyFontWeightsRoboto0 = 'Regular';
+export const typographyFontWeightsInter1 = 'Bold';
+export const typographyFontWeightsInter2 = 'Regular';
